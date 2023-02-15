420fa4f0fb8e7814b049a23991f98a6046cbab97
diyamehta <96303315+diyamehta@users.noreply.github.com>
2023-02-16 00:00:37 +0530

    Update curvature.m

--- curvature.m
+++ curvature.m
@@ -1,6 +1,6 @@
-function delta = curvature(Q,s) %delta=kappa in demonstration paper, eqn 19
+function kappa= curvature(Q,h) %delta=kappa in demonstration paper, eqn 19
     %for i=1:6
-    angle=(1-2*(((Q(2)^2)+(Q(3)^2))))
-    delta = acos(angle)/s; 
+    anglel = (1-2*(((Q(2)^2)+(Q(3)^2))));
+    kappa = acos(anglel)/h;
     %end 
     end 
