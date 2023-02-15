0e343a45cbce7be9c73af0c798f8d5b75f6e4d08
diyamehta <96303315+diyamehta@users.noreply.github.com>
2023-02-16 00:01:31 +0530

    Update angle.m

--- angle.m
+++ angle.m
@@ -1,3 +1,3 @@
-function phi = angle(Q) %eqn 19
-        phi = atan((Q(2)*Q(1)+Q(3)*Q(4))/(Q(2)*Q(4)-Q(3)*Q(1)));
- end 
+function theta = angle(Q) %eqn 19
+        theta = atan((Q(2)*Q(1)+Q(3)*Q(4))/(Q(2)*Q(4)-Q(3)*Q(1)));
+end 
