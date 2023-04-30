a65b64da605fc0aec827b2a931832d7e2eabfc4e
diyamehta <96303315+diyamehta@users.noreply.github.com>
2023-04-30 15:37:17 +0530

    Delete curvature.m

--- curvature.m
+++
@@ -1,6 +0,0 @@
-function kappa= curvature(Q,h) %delta=kappa in demonstration paper, eqn 19
-    %for i=1:6
-    anglel = (1-2*(((Q(2)^2)+(Q(3)^2))));
-    kappa = acos(anglel)/h;
-    %end 
-    end 
