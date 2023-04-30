f56ee85b240fd5aca5abccaee9cc01cbb8b9f113
diyamehta <96303315+diyamehta@users.noreply.github.com>
2023-04-30 15:38:13 +0530

    Delete tippos.m

--- tippos.m
+++
@@ -1,5 +0,0 @@
-function p = tippos(delta, s, phi)
-p(1)= ((delta*(s^2)*cos(phi)*((delta)^2*(s)^2-12)))/24; 
-p(2)= ((delta*(s)^2*sin(phi))*((delta)^2*(s)^2-12))/24;
-p(3)= (s-(delta*(s)^3))/6;
-end 
